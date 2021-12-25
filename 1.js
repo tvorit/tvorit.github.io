c244c9d5ccf28e7cfa8494d6b84ab2c4f33852c4
mihalera <88746771+mihalera@users.noreply.github.com>
2021-12-25 19:24:43 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,89 +1,56 @@
- function gir() {  
+function gir() {  
   nums = document.getElementById('nums_1').innerHTML  
   if(nums == 1) {document.getElementById('gir').className='gir_1';document.getElementById('nums_1').innerHTML='2'}  
   if(nums == 2) {document.getElementById('gir').className='gir_2';document.getElementById('nums_1').innerHTML='3'}  
   if(nums == 3) {document.getElementById('gir').className='gir_3';document.getElementById('nums_1').innerHTML='1'}  
   }  
   setInterval(function(){gir()}, 500)
-const UPPER_LIMIT_Y = 20;
-const UPPER_LIMIT_X = 2;
-const LOWER_LIMIT_X = -2;
-const MAX_SIZE = 6;
-const MIN_SIZE = 2;
-const AMOUNT = 250;
-const COLOR = 0xffffff;
-const { Application, Graphics } = PIXI;
-const floored = v => Math.floor(Math.random() * v);
-const update = (p) =>
-Math.random() > 0.5 ?
-Math.max(LOWER_LIMIT_X, p - 1) :
-Math.min(p + 1, UPPER_LIMIT_X);
+function getTimer( count ){
+    var timer = new Object();
+    timer.seconds = count % 60;
+    count = (count - timer.seconds) / 60;
+    timer.minutes = count % 60;
+    count = (count - timer.minutes) / 60;
+    timer.hours = count % 24;
+    timer.days = (count - timer.hours) / 24;
+    return timer;
+}
 
-const reset = p => {
-  p.x = floored(app.renderer.width);
-  p.y = -(p.height + floored(app.renderer.height));
-  p.vy = floored(UPPER_LIMIT_Y);
-};
+// Вывод таймера
+function showTimer( timer ){
+    $('#timer').text(
+        timer.days.toString() +
+        ':' +
+        (timer.hours < 10 ? '0' : '') +
+        timer.hours.toString() +
+        ':' +
+        (timer.minutes < 10 ? '0' : '') +
+        timer.minutes.toString() +
+        ':' +
+        (timer.seconds < 10 ? '0' : '') +
+        timer.seconds.toString()
+    );
+}
 
-const genParticles = (t) =>
-new Array(AMOUNT).fill().map(p => {
-  const SIZE = floored(MAX_SIZE) + MIN_SIZE;
-  p = new PIXI.Sprite(t);
-  p.scale.x = p.scale.y = SIZE / 100;
-  // p.width = p.height = SIZE
-  p.vx = floored(UPPER_LIMIT_X) - UPPER_LIMIT_X;
-  p.vy = floored(UPPER_LIMIT_Y);
-  p.alpha = Math.random();
-  p.x = floored(app.renderer.width);
-  p.y = -(SIZE + floored(app.renderer.height));
-  drops.addChild(p);
-  return p;
+// поехали
+$(document).ready( function(){
+    var now =     new Date();                                        // Здесь нужно получить точное время
+    var newyear = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0, 0); 
+    var counter = newyear.getTime() - now.getTime();                // милисекунд до НГ
+    var timeout = counter % 1000;                                    // Милисекунды до синхронного вывода целых секунд
+    counter = (counter - timeout) / 1000;                            // Кол-во секунд до Нового Года   
+    showTimer(getTimer(counter + 1));                                // Вывод ближайшей целой секунды
+    setTimeout(function(){
+        showTimer(getTimer(counter));                                // Синхронный вывод 1-й целой секунды
+        var intervalID = setInterval(function(){
+            counter--;
+            if( counter > 0 ){
+                showTimer(getTimer(counter));                        // Синхронный вывод n-й целой секунды
+            }else{
+                clearInterval(intervalID);
+                $('#timer').text('Ура!');
+            }
+        }, 1000);
+    }, timeout);
+    
 });
-
-const app = new Application({
-  antialias: true,
-  transparent: true });
-
-
-const drops = new PIXI.particles.ParticleContainer(AMOUNT, {
-  scale: true,
-  position: true,
-  alpha: true });
-
-app.stage.addChild(drops);
-
-const p = new Graphics();
-p.beginFill(COLOR);
-p.drawCircle(0, 0, 100);
-p.endFill();
-const baseTexture = app.renderer.generateTexture(p);
-let particles = genParticles(baseTexture);
-
-// app.stage.filters = [new PIXI.filters.BlurFilter(3)]
-app.ticker.add(i => {
-  if (
-  app.renderer.height !== innerHeight ||
-  app.renderer.width !== innerWidth)
-  {
-    app.renderer.resize(innerWidth, innerHeight);
-    drops.removeChildren();
-    particles = genParticles(baseTexture);
-  }
-  for (let particle of particles) {
-    if (particle.y > 0) particle.x += particle.vx;
-    particle.y += particle.vy;
-
-    if (Math.random() > 0.9) particle.vx = update(particle.vx);
-    if (Math.random() > 0.9)
-    particle.vy = Math.min(particle.vy + 1, UPPER_LIMIT_Y);
-
-    if (
-    particle.x > app.renderer.width ||
-    particle.x < 0 ||
-    particle.y > app.renderer.height)
-
-    reset(particle);
-  }
-});
-
-document.body.appendChild(app.view);
